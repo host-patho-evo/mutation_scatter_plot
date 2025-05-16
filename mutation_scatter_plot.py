6854c7e330a045eddcd7c525d73007c0a4b9cd4a
Martin Mokrejs <mmokrejs@gmail.com>
2025-05-16 14:48:50 +0200

    Add more links to the dataset on Zenodo
    Provide more examples and URLs for downloads, document more conda
    and pip dependencies in README. Add run times and versions of the
    packages tested during development.
    Add back support for parsing the TSV file format with just 6 columns
    Add a section about incompatible changes in current codebase requiring a patched
    biopython, see pull-request #4992 at https://github.com/biopython/biopython/pull/4992
    Describe what is output on STDOUT.
    Add example --help output for an overview of options to README.

--- mutation_scatter_plot.py
+++ mutation_scatter_plot.py
@@ -113,11 +113,11 @@ setcontext(ExtendedContext)
 c = getcontext()
 c.prec = 99
 
-version = 202504201645
+version = 202505170040
 
 myparser = OptionParser(version="%s version %s" % ('%prog', version))
 myparser.add_option("--tsv", action="store", type="string", dest="tsv_file_path", default='',
-    help="Path to a TAB separated file with 5-columns: ['position', 'original_aa', 'mutant_aa', 'frequency', 'original_codon', 'mutant_codon', 'coverage_per_codon']")
+    help="Path to a TAB separated file with 5-columns: ['position', 'original_aa', 'mutant_aa', 'frequency', 'original_codon', 'mutant_codon', 'coverage_per_codon'] or more up to 11-columns ['padded_position', 'position', 'original_aa', 'mutant_aa', 'frequency', 'original_codon', 'mutant_codon', 'observed_codon_count', 'total_codons_per_site', 'frequency_parent', 'frequency_selected'] as we kept extending the file format")
 myparser.add_option("--column", action="store", type="string", dest="column_with_frequencies", default='frequency',
     help="Name of a column in input TSV to be used for rendering frequencies [frequency]")
 myparser.add_option("--outfile", action="store", type="string", dest="outfile", default='',
@@ -151,7 +151,7 @@ myparser.add_option("--legend", action="store_true", dest="legend", default=Fals
 myparser.add_option("--matrix", action="store", type="int", dest="matrix", default=62,
     help="BLOSUM matrix: 45,50,62,80,90 [default is 62]")
 myparser.add_option("--matrix-file", action="store", type="string", dest="matrix_file", default=None,
-    help="Matrix file compatible with BLOSUM matrices, e.g. prot26050-sup-0004-Supinfo03.sm from https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8641535/bin/NIHMS1664401-supplement-supinfo.rar")
+    help="Matrix file compatible with BLOSUM matrices, e.g. prot26050-sup-0004-Supinfo03.sm from https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8641535/bin/NIHMS1664401-supplement-supinfo.rar if you do not like default BLOSUM62")
 myparser.add_option("--colormap", action="store", type="string", dest="colormap", default='coolwarm_r',
     help="Pick a colormap recognized by matplotlib. See https://i.sstatic.net/cmk1J.png [default is coolwarm_r]")
 myparser.add_option("--dpi", action="store", type="int", dest="dpi", default=600,
@@ -407,7 +407,10 @@ def main():
         print("Info: Autodetected old TSV file format without a header in %s, assigning default column names" % myoptions.tsv_file_path)
         df = pd.read_csv(myoptions.tsv_file_path, sep='\t', header=0)#, nrows=500)
         # Assign column names
+        # released results on Zenodo: 430	T	K	0.000386	ACA	AAA
         try:
+            df.columns = ['position', 'original_aa', 'mutant_aa', 'frequency', 'original_codon', 'mutant_codon']
+        except ValueError:
             df.columns = ['position', 'original_aa', 'mutant_aa', 'frequency', 'original_codon', 'mutant_codon', 'observed_codon_count', 'total_codons_per_site']
         except ValueError:
             df.columns = ['padded_position', 'position', 'original_aa', 'mutant_aa', 'frequency', 'original_codon', 'mutant_codon', 'observed_codon_count', 'total_codons_per_site']
