2dbef3473294caf0ed28828cbea4371776cd896b
Martin Mokrejš <mmokrejs@gmail.com>
2026-04-12 02:38:26 +0200

    Add mutation_timeline_plot.py wrapper in scripts/

---
+++ scripts/mutation_timeline_plot.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8 ts=4 sw=4 expandtab :
+"""Wrapper script to invoke mutation_timeline_plot from the scripts/ directory."""
+
+from mutation_scatter_plot.mutation_scatter_plot.timeline_cli import main
+
+if __name__ == '__main__':
+    main()
